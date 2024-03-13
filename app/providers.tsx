07d003e684a7d21c5c2c575c2b511762873f1c50
mxnan <128995981+mxnan@users.noreply.github.com>
2024-03-13 13:32:03 +0530

    working app with shadcn,nextui,tailwind

---
+++ app/providers.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import * as React from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextUIProvider>
+      <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    </NextUIProvider>
+  );
+}
