fca6b5510de3b3f0f0c7569439a3d50f056292d5
mxnan <128995981+mxnan@users.noreply.github.com>
2024-03-13 22:15:04 +0530

    minor

--- components/ui/ToggleTheme.tsx
+++ components/ui/ToggleTheme.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { motion } from "framer-motion";
 
 import {
   Button,
@@ -13,28 +14,48 @@ import {
 } from "@/components/ui";
 
 export function ToggleTheme() {
-  const { setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <motion.div className="fixed top-20 left-4  flex items-center  ">
+      {theme === "dark" ? (
+        <motion.button
+          animate={{
+            rotate: 360,
+            transition: {
+              duration: 2,
+              repeat: Infinity,
+              repeatType: "reverse",
+              ease: "linear",
+            },
+          }}
+          onClick={() => setTheme("light")}
+        >
+          <Sun className=" w-8 h-8  " />
+        </motion.button>
+      ) : (
+        <motion.button
+          animate={{
+            rotate: 360,
+            transition: {
+              duration: 2,
+              repeat: Infinity,
+              repeatType: "reverse",
+              ease: "linear",
+            },
+          }}
+          onClick={() => setTheme("dark")}
+        >
+          <Moon className=" w-8 h-8   " />
+        </motion.button>
+      )}
+    </motion.div>
   );
 }
