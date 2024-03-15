ffa523b3ec2c2ea38938adba83f7265a1fcf97de
mxnan <128995981+mxnan@users.noreply.github.com>
2024-03-15 16:13:39 +0530

    added svg in pf-section

--- components/sections/performancesection/performance.tsx
+++ components/sections/performancesection/performance.tsx
@@ -3,9 +3,9 @@ import Graphsvg from "./graph-svg";
 
 export default function Performance() {
   return (
-    <div className="flex w-full max-w-6xl mx-auto ">
+    <div className="flex w-full h-[100vh] max-w-6xl mx-auto ">
       
-      <div className="flex overflow-clip h-2/3 rounded-3xl ">
+      <div className="flex overflow-clip max-w-5xl mx-auto h-max rounded-3xl ">
         <Graphsvg />
       </div>
     </div>
