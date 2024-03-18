c3bfb9906f1781cef466c95bd1e0294d358d8f7e
mxnan <128995981+mxnan@users.noreply.github.com>
2024-03-18 23:40:45 +0530

    performance animated

--- components/sections/performancesection/performance-text.tsx
+++ components/sections/performancesection/performance-text.tsx
@@ -1,28 +1,68 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
+
+const containervariants = {
+  animate: {
+    transition: {
+      staggerChildren: 0.6, // Adjust the stagger duration as needed
+    },
+  },
+};
+const textvariants = {
+  initial: {
+    opacity: 0,
+    y: -100,
+    x: 300,
+  },
+  animate: {
+    opacity: 1,
+
+    y: 0,
+    x: 0,
+    transition: {
+      duration: 1,
+      type: "spring",
+      ease: "easeInOut",
+    },
+  },
+};
 
 export default function Texttop() {
   return (
-    <>
+    <motion.div
+      variants={containervariants}
+      initial="initial"
+      animate="animate"
+      className="space-y-10"
+    >
+      {" "}
       <div className="w-full flex flex-col gap-8 text-center">
-        <p className="text-5xl max-md:text-4xl font-logo ">
+        <motion.p
+          variants={textvariants}
+          className="text-5xl max-md:text-4xl font-logo "
+        >
           real world software engineering performance
-        </p>
-        <p className="text-3xl font-logo">[SWE-Bench]</p>
+        </motion.p>
+        <motion.p variants={textvariants} className="text-3xl font-logo">
+          [SWE-Bench]
+        </motion.p>
       </div>
-
       <div className="space-y-8 font-medium px-6 ">
-        <p className="font-logo text-3xl ">{"Devin's Performance :"}</p>
-        <p className="text-lg">
+        <motion.p variants={textvariants} className="font-logo text-3xl ">
+          {"Devin's Performance :"}
+        </motion.p>
+        <motion.p variants={textvariants} className="text-lg">
           {
             "We evaluated Devin on SWE-bench, a challenging benchmark that asks agents to resolve real-world GitHub issues found in open source projects like Django and scikit-learn."
           }
-        </p>
-        <p className="text-lg ">
+        </motion.p>
+        <motion.p variants={textvariants} className="text-lg ">
           {
             "Devin correctly resolves 13.86%* of the issues end-to-end, far exceeding the previous state-of-the-art of 1.96%. Even when given the exact files to edit, the best previous models can only resolve 4.80% of issues."
           }
-        </p>
+        </motion.p>
       </div>
-    </>
+    </motion.div>
   );
 }
--- components/sections/performancesection/performance.tsx
+++ components/sections/performancesection/performance.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import GraphContent from "./performance-graph";
 import Image from "next/image";
 import Texttop from "./performance-text";
+import TypewriterParagraph from "@/components/ui/typewriter-word";
 
 export default function Performance() {
   return (
     <div className="w-full h-full  px-2 max-w-6xl pb-4 border-b space-y-20 max-md:space-y-16 mx-auto ">
       <Texttop />
+      {/* <GraphContent /> */}
       <GraphContent />
       <Image
         src="/devingraph.png"
@@ -15,12 +17,17 @@ export default function Performance() {
         height={1000}
         className="w-full md:hidden h-full rounded-3xl shadow-2xl border"
       />
+      {/* <GraphContent /> */}
 
-      <p className="text-2xl max-md:text-xl py-4 text-center font-logo hover:underline underline-offset-4 font-medium">
-        {
-          "We plan to publish a more detailed technical report soon — stay tuned for more details."
-        }
-      </p>
+      <div className="text-2xl max-md:text-xl py-4 text-center font-logo hover:underline underline-offset-4 font-medium">
+        <TypewriterParagraph
+          text={
+            "We plan to publish a more detailed technical report soon — stay tuned for more details."
+          }
+          delay={0.2}
+          speed={0.2}
+        />
+      </div>
     </div>
   );
 }
